73fed4a26f339acd50c01dd25c336d9a281a5f5a
Meseery <m.elmeseery@meunity.com>
2017-05-19 20:59:07 +0200

    Initial Commit

---
+++ MMNetworkingKit/MMNetworkingKit.h
@@ -0,0 +1,19 @@
+//
+//  MMNetworkingKit.h
+//  MMNetworkingKit
+//
+//  Created by Mohamed EL Meseery on 5/19/17.
+//  Copyright © 2017 Meseery. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MMNetworkingKit.
+FOUNDATION_EXPORT double MMNetworkingKitVersionNumber;
+
+//! Project version string for MMNetworkingKit.
+FOUNDATION_EXPORT const unsigned char MMNetworkingKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MMNetworkingKit/PublicHeader.h>
+
+
